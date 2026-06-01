127a00ce576c16023bfb8ac1a151640e4d6c4ab3
Bando Masaya <union.septem@gmail.com>
2026-06-01 23:59:36 +0900

    Initial Commit

---
+++ CaffeinateUI/CaffeinateUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffeinateUIApp.swift
+//  CaffeinateUI
+//
+//  Created by Masaya Bando on 2026/06/01.
+//
+
+import SwiftUI
+
+@main
+struct CaffeinateUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaffeinateUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaffeinateUI
+//
+//  Created by Masaya Bando on 2026/06/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
